ce0e897b53c3a7c1ec6fa7ccab06c84e5bce130c
Cameron Smith <71192028+smith-cameron@users.noreply.github.com>
2023-07-22 09:21:02 -0700

    option to not copy git_links.html

--- xyz.py
+++ xyz.py
@@ -1,15 +1,20 @@
 import os, sys, subprocess, traceback, webbrowser, shutil
 from zipfile import ZipFile
 
-assignment_name = input("Assignment Name or Alias: ")
+def set_descision(user_input):
+    return True if user_input.lower() == 'y' else False
+
+need_links = set_descision(input("Include Git Repo Links? (Y/yes, Any Key/no "))
+open_links = set_descision(input("Open Git Repo Links? (Y/yes, Any Key/no "))
+if need_links:
+    assignment_name = input("Assignment Name or Alias: ")
 zipped_parent = input("FILE PATH to ZIPPED DOWNLOADED assignemnt directory(where is it?): \n")
 location_option = input("To unzip files into containing directory enter Y... or nothing...\n   *OR*\nPlease provide FILE PATH to destination directory(where is it going?): ")
-possible_input = ['nothing', 'none', 'nada', 'y', '']
 
-if location_option.lower() in possible_input:
-    destination_path = None
-else:
-    destination_path = location_option
+
+def set_location(location_option):
+    possible_input = ['nothing', 'none', 'nada', 'y', '']
+    return None if location_option.lower() in possible_input else location_option
 
 def if_group(dir_path):
     if not os.path.exists(dir_path):
@@ -26,19 +31,24 @@ def scan_assignments(incoming, destination_path):
         file_name = os.fsdecode(file)
         download_dir = os.path.join(incoming, file_name)
         student_dir = os.path.join(destination_path, file_name)
-        if file_name.replace(' ', '').endswith(".html"):
-            new_name = os.path.join(destination_path, f"{assignment_name}_gitLinks.html")
-            print(f"\nCopying File:\n{file_name}\nTo: {new_name}")
-            shutil.copy(download_dir, new_name)
-            # open_links(new_name) #Opens github repos listed in _gitLinks.html
-            webbrowser.open_new(new_name) #Opens _gitLinks.html
-            continue
+        if file_name.replace(' ', '').endswith(".html") and need_links:
+            new_name = copy_links(download_dir, file_name)
+            if open_links:
+                # open_links(new_name) #Opens github repos listed in _gitLinks.html
+                webbrowser.open_new(new_name) #Opens _gitLinks.html
+                continue
         if is_filtered_file(file_name):
             print(f"Skipping File:\n{file_name}\n")
             continue
         if_group(student_dir)
         open_child(download_dir, student_dir, file_name)
     destroy_temp(incoming)
+
+def copy_links(download_dir, file_name):
+    new_name = os.path.join(destination_path, f"{assignment_name}_gitLinks.html")
+    print(f"\nCopying File:\n{file_name}\nTo: {new_name}")
+    shutil.copy(download_dir, new_name)
+    return new_name
 
 def is_filtered_file(file_name):
     filtered_extensions = ['.metadata', '_MACOSX', '.git', 'node_modules', '.DS_Store']
@@ -93,6 +103,7 @@ def open_links(filepath):
     print()
 
 try:
+    destination_path = set_location(location_option)
     scan_assignments(open_parent(zipped_parent), trim_filepath(destination_path))
     destroy_temp(zipped_parent)
 except Exception as e:
